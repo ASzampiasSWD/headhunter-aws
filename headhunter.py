79984898c53b9b4871c787d7e3c8e03d8fad5c2f
Amanda Szampias <aszampias@Amandas-MacBook-Air.local>
2022-06-19 13:14:49 -0500

    docs

--- headhunter.py
+++ headhunter.py
@@ -5,7 +5,7 @@ import os
 import colorama
 from termcolor import colored
 from pathlib import Path
-from rekognition_objects import RekognitionFace
+#from rekognition_objects import RekognitionFace
 from PIL import Image
 import io
 
@@ -23,9 +23,9 @@ colorama.init()
 client=boto3.client('rekognition')
 maxFaces=3
 
-parser = argparse.ArgumentParser(description='Find face matches from one image.')
+parser = argparse.ArgumentParser(description='Find face matches from one image or collection.')
 parser.add_argument('target_resource', type=str,
-                    help='path of the person image you want found')
+                    help='path of the person image or collection name you want found')
 parser.add_argument('-st', '--similarity-threshold', dest='similarity_threshold', type=int,
                     default=70, help='Minimum threshold allowed for face matches. Default is 70.')
 parser.add_argument('-c', '--compare-directory', dest='compare_directory', type=str, default=None,
@@ -34,6 +34,10 @@ parser.add_argument('-o', '--output-file', dest='output_file_name', type=str, de
                     help='Name of the output file. Default is success.txt')
 parser.add_argument('-s', '--start-at', dest='start_at', type=int, default=None,
                     help='Need to start at the middle of a directory? Input the file number here. 5 = 5th Image')
+parser.add_argument('-dl', '--detect-labels', dest='detect_labels', action="store_true",
+                    help='Detect Labels in one image')
+parser.add_argument('-ocr', '--ocr', dest='detect_text', action="store_true",
+                    help='Detect Text in one image')
 
 args = parser.parse_args()
 successFile = open(args.output_file_name,'a')
@@ -118,15 +122,63 @@ def cropBoundingBox(photo, box):
     #cropFace.show()
     return cropFace
 
+def detectLabels(photo):
+    img = openImageFile(photo)
+    response = client.detect_labels(Image={'Bytes': img.read()}, MaxLabels=10)
+
+    print('Detected labels for: {}'.format(photo)) 
+    for label in response['Labels']:
+        print ("Label: {}".format(label['Name']))
+        print ("Confidence: ".format(label['Confidence']))
+        print ("Instances:")
+        for instance in label['Instances']:
+            print ("  Bounding box")
+            print ("    Top: {}".format(instance['BoundingBox']['Top']))
+            print ("    Left: {}".format(instance['BoundingBox']['Left']))
+            print ("    Width: {}".format(instance['BoundingBox']['Width']))
+            print ("    Height: {}".format((instance['BoundingBox']['Height'])))
+            print ("  Confidence: {}".format(instance['Confidence']))
+            print()  
+        for parent in label['Parents']:
+            print ("Parents:")
+            print ("   ".format(parent['Name']))
+        print ("----------")
+    return len(response['Labels'])
+
+def detectText(photo):
+    img = openImageFile(photo)
+    response = client.detect_text(Image={'Bytes': img.read()})
+                        
+    textDetections=response['TextDetections']
+    print ('Detected text\n----------')
+    for text in textDetections:
+            print('Detected text: {}'.format(text['DetectedText']))
+            print('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+            print('Id: {}'.format(text['Id']))
+            if ('ParentId' in text):
+                print ('Parent Id: {}'.format(text['ParentId']))
+            print('Type: {}'.format(text['Type']))
+    return len(textDetections)
+
+
 if (args.start_at != None):
   intFileIndex = args.start_at
 
-arImageFiles = getImageFilesFromDirectory()
-print('Total Images in Processing: {}'.format(len(arImageFiles)))
+if (args.detect_labels == False and args.detect_text == False):
+  arImageFiles = getImageFilesFromDirectory()
+  print('Total Images in Processing: {}'.format(len(arImageFiles)))
 
 for imageName in getImageFilesFromDirectory():
-  if ('.' in args.target_resource):
+  if ('.' in args.target_resource and args.detect_labels == False and args.detect_text == False):
     compareFaces(args.target_resource, imageName)
+  elif ('.' in args.target_resource and args.detect_text == True):
+    detectText(args.target_resource)
+    print(colored('1 Image Processed', 'green'))
+    exit()
+  elif ('.' in args.target_resource and args.detect_labels == True):
+    detectLabels(args.target_resource)
+    print(colored('1 Image Processed', 'green'))
+    exit()
   else:
     arCroppedFaces = detectFacesInImage(imageName)
     bFindPerson = False
